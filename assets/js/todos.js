2fd58f1f1ea941f2e7d9679bb9ff9ef3c47034a0
Jatin Sadhwani <jatinsadhwani94@gmail.com>
2018-05-21 21:17:56 -0400

    delete todo added

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,18 +1,16 @@
 //check off specific todos by clicking
 $("li").click(function(){
-	
 	//if li is grey, turn it black
 	//else turn it grey
-	if($(this).css("color") === "rgb(128, 128, 128)"){
-		$(this).css({
-			color: "black",
-			textDecoration: 'none'
-		});	
-	}
-	else{
-		$(this).css({
-			color: "grey",
-			textDecoration: "line-through"
-		});
-	}
+	$(this).toggleClass("completed");
 });
+
+//click on X to delete todo
+
+$("span").click(function(event){
+	$(this).parent().fadeOut(1000,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
+
