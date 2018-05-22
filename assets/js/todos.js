a53b04e1c638c960e68a2047c471ca288a3aa1d3
Jatin Sadhwani <jatinsadhwani94@gmail.com>
2018-05-21 21:31:09 -0400

    functionality done

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,5 +1,5 @@
 //check off specific todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li" ,function(){
 	//if li is grey, turn it black
 	//else turn it grey
 	$(this).toggleClass("completed");
@@ -7,10 +7,20 @@ $("li").click(function(){
 
 //click on X to delete todo
 
-$("span").click(function(event){
-	$(this).parent().fadeOut(1000,function(){
+$("ul").on("click","li span", function(event){
+	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
 })
 
+//new todo creation
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13 && $(this).val()!=""){
+		//grabbing new todo text from input
+		var text  = $(this).val();
+		//create a new li and add to ul
+		$("ul").append("<li><span> X </span>"+text+"</li>");
+		$(this).val("");
+	}
+})
